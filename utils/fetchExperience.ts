2609a5194da10cca5189a6b71a5a5f506bc93485
unknown <ogbonnayakingsike@gmail.com>
2023-03-22 08:27:23 -0700

    update

--- utils/fetchExperience.ts
+++ utils/fetchExperience.ts
@@ -1,9 +1,14 @@
 import { Experience } from "../typings";
+// import axios from "axios"
 
 export const fetchExperiences = async () => {
+    // const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`)
     const res = await fetch('http://localhost:3000/api/getExperience')
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`)
+    // console.log(res.data)
     const data = await res.json();
     const experiences: Experience[] = data.experiences
+
 
     return experiences;
 }
--- utils/fetchSkills.ts
+++ utils/fetchSkills.ts
@@ -1,8 +1,9 @@
-import skill from "@/portfolio-kingsike/schemas/skill";
 import { Skill } from "../typings";
 
 export const fetchSkills = async () => {
     const res = await fetch('http://localhost:3000/api/getSkills')
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`)
+    // 
     const data = await res.json();
     const skills: Skill[] = data.skills
 
